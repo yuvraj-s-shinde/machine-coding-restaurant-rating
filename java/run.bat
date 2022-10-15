74f0ace3845d9c5279b502b5dad57f93db49d10c
Yakshit Crio.Do <yakshit@criodo.com>
2022-10-15 13:42:13 +0550

    updated changes - 2022-10-15 13:22:13

--- java/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: .\gradlew clean build -x test --no-daemon
-:: java -jar build\libs\example.jar sample_input\sample_input_one.txt
-
-:: or
-.\gradlew run -q --args="INPUT_FILE=sample_input\sample_input_one.txt"
--- python/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-pip install -r requirements.txt
-python -m app INPUT_FILE=sample_input/sample_input_one.txt
